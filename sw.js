edef656bdbe5feb682e5d2d62714617c48b08acb
Wei Zhang <sheepchina@hotmail.com>
2020-01-08 14:17:22 -0600

    Add functions to html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-contentOfCaches = ['/*.html', '/static/css/*.css', '/examples/*.png'];
+contentOfCaches = 'caches-v1';
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open(contentOfCaches).then(function(cache) {
@@ -38,6 +38,14 @@ self.addEventListener('install', function(event) {
             return caches.delete(contentOfCaches);
           })
         );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
       })
     );
   });
